d45d20085f1efc04dd6f98708c8fc35ebd3bed58
Isaac Villicana <villicana.i@gmail.com>
2018-04-18 03:20:30 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,12 @@
 class Owner
-  attr_accessor :name
+  attr_accessor :name, :all
   attr_reader :species
-
+  @@all = []
   def initialize (species)
     @species = species
   end
 
   def say_species
-    
+
   end
 end
