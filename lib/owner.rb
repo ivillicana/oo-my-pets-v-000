7551a294ae84073f582ff7ea26cfaade81ba2beb
Isaac Villicana <villicana.i@gmail.com>
2018-04-18 04:09:46 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,4 +28,12 @@ class Owner
   def buy_fish(name)
     @pets[:fishes] << Fish.new(name)
   end
+
+  def buy_cat(name)
+    @pets[:cats] << Cats.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dogs.new(name)
+  end
 end
