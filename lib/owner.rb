5dd8afdbb13e0479f2cdd93e177f89a473c67109
Isaac Villicana <villicana.i@gmail.com>
2018-04-18 03:43:17 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -6,7 +6,7 @@ class Owner
   def initialize (species)
     @species = species
     @@all << self
-    @pets = {:fishes => [], :dogs => [], :cats => []}
+    self.pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
   def self.all
